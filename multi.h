1f034a9581cc4921a9f04a6d9809ae966e03e8cb
Marc Balmer <marc.balmer@arcapos.swiss>
2020-05-07 13:02:34 +0200

    sync with upstream repository

--- multi.h
+++ multi.h
@@ -34,6 +34,7 @@ extern int lcurl_multi_init(lua_State *);
 extern int lcurl_multi_add_handle(lua_State *);
 extern int lcurl_multi_fds(lua_State *);
 extern int lcurl_multi_perform(lua_State *);
+extern int lcurl_multi_timeout(lua_State *);
 extern int lcurl_multi_remove_handle(lua_State *);
 
 extern int lcurl_multi_gc(lua_State *);
